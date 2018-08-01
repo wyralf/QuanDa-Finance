98525d4f5064b58d790e047791b2607a80bcac42
张宇飞 <yufei@douyu.tv>
2018-08-01 18:55:55 +0800

    接入环信SDK

--- app/src/main/java/com/tome/android/activity/SplashActivity.java
+++ app/src/main/java/com/tome/android/activity/SplashActivity.java
@@ -6,6 +6,8 @@ import com.tome.android.R;
  * Created by zhangyufei
  */
 public class SplashActivity extends BaseActivity{
+
+    private static final int DELAY_TIME = 2000;
     @Override
     public int getLayoutRes() {
         return R.layout.activity_splash;
