b5aaae6e87cca773c79c606f450c9fb323a39412
wangdandan <1003441962@qq.com>
2018-08-02 00:22:48 +0800

    创建聊天Activity

---
+++ app/src/main/java/com/tome/android/activity/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.tome.android.activity;
+
+public class ChatActivity extends BaseActivity{
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return 0;
+    }
+}
